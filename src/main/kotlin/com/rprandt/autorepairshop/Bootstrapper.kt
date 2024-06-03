d9939e80ddd3444bfb854a811937c4b3403644d6
Richard Prandt <rprandt.contato@gmail.com>
2024-06-02 21:13:42 -0300

    chore: add Bootstrapper component

---
+++ src/main/kotlin/com/rprandt/autorepairshop/Bootstrapper.kt
@@ -0,0 +1,56 @@
+package com.rprandt.autorepairshop
+
+import com.rprandt.autorepairshop.costumer.Costumer
+import com.rprandt.autorepairshop.costumer.CostumerRepository
+import com.rprandt.autorepairshop.vehicle.Vehicle
+import com.rprandt.autorepairshop.vehicle.VehicleRepository
+import com.rprandt.autorepairshop.vehicle.VehicleType
+import org.springframework.context.ApplicationListener
+import org.springframework.context.event.ContextRefreshedEvent
+import org.springframework.stereotype.Component
+
+@Component
+class Bootstrapper(
+    val costumerRepository: CostumerRepository,
+    val vehicleRepository: VehicleRepository,
+): ApplicationListener<ContextRefreshedEvent> {
+    override fun onApplicationEvent(event: ContextRefreshedEvent) {
+        val costumers: List<Costumer> = listOf(
+            Costumer(
+                id = 1,
+                name = "Jose da Silva",
+                email = "jose@email.com",
+            ),
+            Costumer(
+                id = 2,
+                name = "Richard Prandt",
+                email = "richard@email.com",
+            ),
+            Costumer(
+                id = 3,
+                name = "Eliza Podesta",
+                email = "eliza@email.com",
+            )
+        )
+
+        costumerRepository.saveAll(costumers)
+
+        val vehicles: List<Vehicle> = listOf(
+            Vehicle(
+                numberPlate = "ABC-123",
+                costumer = costumers[0],
+                type = VehicleType.CAR,
+                manufactureDate = "212121212"
+            ).also { costumers[0].vehicles.add(it) },
+            Vehicle(
+                numberPlate = "MGP-9900",
+                costumer = costumers[2],
+                type = VehicleType.BIKE,
+                manufactureDate = "121212"
+            ).also { costumers[2].vehicles.add(it) }
+        )
+
+        vehicleRepository.saveAll(vehicles)
+        costumerRepository.saveAll(costumers)
+    }
+}
